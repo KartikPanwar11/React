e4562feb2eee3e61af00609b037692dae3646a7c
KartikPanwar11 <kartikpanwar1101@gmail.com>
2026-08-04 22:25:01 +0530

    Refactor JSX structure and update render method in index.js

--- React Foundation/index.js
+++ React Foundation/index.js
@@ -4,9 +4,18 @@ import ReactDOM from "react-dom/client";
 //React Element
 
 // const heading = React.createElement('h1',{id:"heading"},'Hello from React Element');
-const jsxHeading = <h1 id="heading">Hello from JSX!</h1>
+const Title = () => (
+    <h1 id="heading">Hello from React Component</h1>
+);
+const JsxHeading = () => 
+ (
+ <div id='newHeading'>
+    <Title/>
+ <h1 id="heading">Hello from JSX!</h1>
+ </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(jsxHeading);
+root.render(<JsxHeading/>);
 
-console.log(jsxHeading);
+console.log(<JsxHeading/>);
