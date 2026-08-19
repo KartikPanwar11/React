097f0932e2bc077419f4a99fbc0be0627a1145aa
KartikPanwar11 <kartikpanwar1101@gmail.com>
2026-08-19 18:41:02 +0530

    Add Class Based Component (CBC) implementation

---
+++ Class Based Comp/CBC.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class CBC extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Class Based Component</h1>
+            </div>
+        );
+    }
+}
+
+export default CBC;
