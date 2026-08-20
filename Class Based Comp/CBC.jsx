7729565db1abb1dd9fdd6c6dcc5182029958c4b9
KartikPanwar11 <kartikpanwar1101@gmail.com>
2026-08-20 21:18:48 +0530

    Add Parent component to encapsulate CBC and pass props

--- Class Based Comp/CBC.jsx
+++ Class Based Comp/CBC.jsx
@@ -4,13 +4,29 @@ class CBC extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            count: 0
+        };
+
+
     }
 
 
+
     render() {
+        const{ name, location } = this.props;
+        const {count} = this.state;
+
+
         return (
             <div>
                 <h1>Class Based Component</h1>
+                <p>Name: {name}</p>
+                <p>Location: {location}</p>
+                <h2>Count: {count}</h2>
+                <button onClick={() => this.setState({count: count + 1})}>Increment</button>
+                <button onClick={() => this.setState({count: count - 1})}>Decrement</button>
             </div>
         );
     }
---
+++ Class Based Comp/Parent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CBC from './CBC'
+
+const Parent = () => {
+  return (
+    <div>
+        <h1>Parent</h1>
+        <CBC name={"Fiery"} location={"India"}/>
+    </div>
+  )
+}
+
+export default Parent
